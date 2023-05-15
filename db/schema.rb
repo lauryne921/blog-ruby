22b2ea500b997950054bfbb69c08eeeb59b2dc87
Lauryne Tranchant <laurynetranchant@mbp-de-jonathan.home>
2023-05-15 18:19:22 +0200

    Switch from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2023_05_12_143853) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
